ab6ad2740517a4f762383f8ed1d480b6968bf8b7
balochCoder <raheelahmed017@gmail.com>
2023-12-21 14:17:54 +0400

    Board

---
+++ actions/create-board/index.ts
@@ -0,0 +1,70 @@
+"use server";
+
+import { auth } from "@clerk/nextjs";
+import { InputType, ReturnType } from "./types";
+import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+import { createSafeAction } from "@/lib/create-safe-action";
+import { CreateBoard } from "./schema";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { title, image } = data;
+
+  let board;
+
+  const [imageId, imageThumbUrl, imageFullUrl, imageLinkHTML, imageUserName] =
+    image.split("|");
+
+  console.log({
+    imageId,
+    imageThumbUrl,
+    imageFullUrl,
+    imageLinkHTML,
+    imageUserName,
+  });
+
+  if (
+    !imageId ||
+    !imageThumbUrl ||
+    !imageFullUrl ||
+    !imageUserName ||
+    !imageLinkHTML
+  ) {
+    return {
+      error: "Missing fields. Failed to create board.",
+    };
+  }
+
+  try {
+    board = await db.board.create({
+      data: {
+        title,
+        orgId,
+        imageId,
+        imageThumbUrl,
+        imageFullUrl,
+        imageUserName,
+        imageLinkHTML,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Internal Error",
+    };
+  }
+
+  revalidatePath(`/board/${board.id}`);
+  return {
+    data: board,
+  };
+};
+
+export const createBoard = createSafeAction(CreateBoard, handler);
--- app/(platfrom)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platfrom)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,11 +1,20 @@
-
+import { Suspense } from "react";
+import { Separator } from "@/components/ui/separator";
+import { Info } from "./_components/info";
+import { BoardList } from "./_components/board-list";
 
 const OrganizationPage = () => {
   return (
-    <div>
-      Organization Page
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <Suspense fallback={<BoardList.Skeleton />}>
+          <BoardList />
+        </Suspense>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default OrganizationPage
+export default OrganizationPage;
