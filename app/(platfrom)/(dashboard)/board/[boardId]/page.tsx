c07ef8ba0df63066f6b0f915a7d682c7a6fd08e7
balochCoder <raheelahmed017@gmail.com>
2023-12-22 18:29:37 +0400

    changes

--- app/(platfrom)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platfrom)/(dashboard)/board/[boardId]/page.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { ListContainer } from "./_components/list-container";
 
-const BoardPage = () => {
-  return (
-    <div className='w-full'>BoardPage</div>
+interface BoardPageProps {
+  params: {
+    boardId: string;
+  };
+};
+
+const BoardPage = async ({
+  params,
+}: BoardPageProps) => {
+
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+  const lists = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
+  return ( 
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer boardId={params.boardId} data={lists}/>
+    </div>
   )
 }
 
